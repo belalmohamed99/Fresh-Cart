736daf64169a2b17eeee4b4aeac2b6a9543abb9e
belalmohamed99 <121451582+belalmohamed99@users.noreply.github.com>
2025-04-26 19:40:01 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,26 +25,26 @@ export const routes: Routes = [
     loadComponent: () => import("./core/layout/blank/blank.component").then(c => c.BlankComponent),
     children: [
       { path: "", redirectTo: "home", pathMatch: "full" },
-      { path: "home", loadComponent: () => import("./feature/pages/home/home.component").then(c => c.HomeComponent), title: "Home" },
-      { path: "cart", loadComponent: () => import("./feature/pages/cart/cart.component").then(c => c.CartComponent), title: "Cart" },
-      { path: "categories", loadComponent: () => import("./feature/pages/categories/categories.component").then(c => c.CategoriesComponent), title: "Categories" },
-      { path: "brands", loadComponent: () => import("./feature/pages/brands/brands.component").then(c => c.BrandsComponent), title: "Brands" },
-      { path: "products", loadComponent: () => import("./feature/pages/products/products.component").then(c => c.ProductsComponent), title: "Products" },
-      { path: "wishlist", loadComponent: () => import("./feature/pages/wishlist/wishlist.component").then(c => c.WishlistComponent), title: "Wishlist" },
-      { path: "allorders", loadComponent: () => import("./feature/pages/allorders/allorders.component").then(c => c.AllordersComponent), title: "All Orders" },
+      { path: "home", loadComponent: () => import("./featur/pages/home/home.component").then(c => c.HomeComponent), title: "Home" },
+      { path: "cart", loadComponent: () => import("./featur/pages/cart/cart.component").then(c => c.CartComponent), title: "Cart" },
+      { path: "categories", loadComponent: () => import("./featur/pages/categories/categories.component").then(c => c.CategoriesComponent), title: "Categories" },
+      { path: "brands", loadComponent: () => import("./featur/pages/brands/brands.component").then(c => c.BrandsComponent), title: "Brands" },
+      { path: "products", loadComponent: () => import("./featur/pages/products/products.component").then(c => c.ProductsComponent), title: "Products" },
+      { path: "wishlist", loadComponent: () => import("./featur/pages/wishlist/wishlist.component").then(c => c.WishlistComponent), title: "Wishlist" },
+      { path: "allorders", loadComponent: () => import("./featur/pages/allorders/allorders.component").then(c => c.AllordersComponent), title: "All Orders" },
       {
         path: "setting",
-        loadComponent: () => import("./feature/pages/profile-setting/profile-setting.component").then(c => c.ProfileSettingComponent),
+        loadComponent: () => import("./featur/pages/profile-setting/profile-setting.component").then(c => c.ProfileSettingComponent),
         title: "Setting",
         children: [
           { path: "", redirectTo: "account-setting", pathMatch: "full" },
-          { path: "account-setting", loadComponent: () => import("./feature/pages/profile-setting/components/account-setting/account-setting.component").then(c => c.AccountSettingComponent) },
-          { path: "privacy-setting", loadComponent: () => import("./feature/pages/profile-setting/components/privacy-setting/privacy-setting.component").then(c => c.PrivacySettingComponent) },
+          { path: "account-setting", loadComponent: () => import("./featur/pages/profile-setting/components/account-setting/account-setting.component").then(c => c.AccountSettingComponent) },
+          { path: "privacy-setting", loadComponent: () => import("./featur/pages/profile-setting/components/privacy-setting/privacy-setting.component").then(c => c.PrivacySettingComponent) },
         ]
       },
-      { path: "checkout/:id", loadComponent: () => import("./feature/pages/checkout/checkout.component").then(c => c.CheckoutComponent), title: "Checkout" },
-      { path: "product-details/:id", loadComponent: () => import("./feature/pages/product-detalis/product-detalis.component").then(c => c.ProductDetalisComponent), title: "Product Details" },
-      { path: "**", loadComponent: () => import("./feature/pages/not-found/not-found.component").then(c => c.NotFoundComponent), title: "Not Found Page" },
+      { path: "checkout/:id", loadComponent: () => import("./featur/pages/checkout/checkout.component").then(c => c.CheckoutComponent), title: "Checkout" },
+      { path: "product-detalis/:id", loadComponent: () => import("./featur/pages/product-detalis/product-detalis.component").then(c => c.ProductDetalisComponent), title: "Product Details" },
+      { path: "**", loadComponent: () => import("./featur/pages/not-found/not-found.component").then(c => c.NotFoundComponent), title: "Not Found Page" },
     ]
   }
 ];
